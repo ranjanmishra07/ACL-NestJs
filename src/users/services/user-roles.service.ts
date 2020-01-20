f42eebe34a167876d83f6990364c35e2e801bbb4
ashishkr96 <ashishch196@gmail.com>
2020-01-20 20:57:08 +0530

    User role table created

--- src/users/services/user-roles.service.ts
+++ src/users/services/user-roles.service.ts
@@ -1,4 +1,4 @@
-import {Injectable} from '@nestjs/common';
+import { Injectable } from '@nestjs/common';
 import { Role } from '../entities/role.entity';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
@@ -7,21 +7,28 @@ import { User } from '../entities/user.entity';
 
 @Injectable()
 export class UserRoleService {
-  constructor (
+  constructor(
     @InjectRepository(UserRole)
     private readonly userRolesRepositoty: Repository<UserRole>,
     @InjectRepository(User)
     private readonly userRepository: Repository<User>,
     @InjectRepository(Role)
     private readonly roleRepository: Repository<Role>,
-  ) {}
+  ) { }
 
   async createUserRoles(roleIds: number[], userId: number) {
     const roles = await this.roleRepository.findByIds(roleIds);
     const user = await this.userRepository.findOne(userId);
     for (const role of roles) {
-      const userRole = {role, user};
-      await this.userRolesRepositoty.insert(userRole);
+      // // const userRole = {role, user};
+      // const userRole = new UserRole();
+      // userRole.role = roleIds;
+      const userRole = new UserRole();
+      userRole.role = role;
+      userRole.user = user;
+      console.log('role', userRole);
+      await this.userRolesRepositoty.save(userRole);
+
     }
   }
 }
